1f54392f6b93033c55def76ecb8446f06115dd5b
washbin <76929116+washbin@users.noreply.github.com>
2023-01-22 15:27:51 +0545

    chore: rename documentation route

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
       methodKey,
   };
   const document = SwaggerModule.createDocument(app, config, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
   console.log(`Applicaiton is running on: ${await app.getUrl()}`);
